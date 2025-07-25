f6c1e9c73402f38af9ab6822e2d273618380a56c
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-07-25 14:20:37 +0200

    RegisterTargetMachine hook works with custom-backend-shlib patch on release/20.x

---
+++ test/lit.cfg.py
@@ -0,0 +1,25 @@
+import os
+import lit.formats
+
+config.name = 'TPDE-SHLIB'
+config.test_format = lit.formats.ShTest(True)
+
+# Describe test files
+config.suffixes = ['.cpp']
+config.excludes = ["Inputs"]
+
+config.test_source_root = os.path.dirname(__file__)
+config.test_exec_root = os.path.join(config.llvm_build_dir, "tools", "tpde-shlib", "test")
+
+config.substitutions.append(('%tpde_shlib', config.tpde_shlib))
+config.substitutions.append(('%clang', 'clang++'))
+
+import os
+config.environment['PATH'] = os.path.pathsep.join((config.llvm_tools_dir, config.environment.get('PATH', '')))
+
+config.available_features.add('shell')
+if hasattr(config, 'enable_plugins') and config.enable_plugins:
+    config.available_features.add('plugins')
+
+if config.build_type == "Debug":
+    config.available_features.add('llvm-debug')
--- tpde-shlib.cpp
+++ tpde-shlib.cpp
@@ -7,41 +7,42 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
 #include "llvm/Target/TargetOptions.h"
+#include "llvm/Target/X86/X86TargetMachine.h"
 
 #include <optional>
 
 using namespace llvm;
 
-class X86TargetMachineTPDE : public TargetMachine {
+class X86TargetMachineTPDE : public X86TargetMachine {
 public:
   X86TargetMachineTPDE(const Target &T, const Triple &TT, StringRef CPU,
                        StringRef FS, const TargetOptions &Options,
                        std::optional<Reloc::Model> RM,
                        std::optional<CodeModel::Model> CM, CodeGenOptLevel OL,
                        bool JIT)
-      : TargetMachine(T, "", TT, CPU, FS, Options) {}
+      : X86TargetMachine(T, TT, CPU, FS, Options, RM, CM, OL, JIT) {}
 
   bool
   addPassesToEmitFile(PassManagerBase &PM, raw_pwrite_stream &Out,
                       raw_pwrite_stream *DwoOut, CodeGenFileType FileType,
                       bool DisableVerify = true,
                       MachineModuleInfoWrapperPass *MMIWP = nullptr) override {
-    // success
-    return false;
+    printf("Adding custom passes");
+    return false; // success
   }
 };
 
 namespace llvm {
 
-Target &getTheX86_32Target();
-Target &getTheX86_64Target();
+LLVM_ABI Target &getTheX86_32Target();
+LLVM_ABI Target &getTheX86_64Target();
 
 } // namespace llvm
 
 struct PatchLLVMX86TargetOnLoad {
   PatchLLVMX86TargetOnLoad() {
-    llvm::RegisterTargetMachine<X86TargetMachineTPDE>(getTheX86_32Target());
-    llvm::RegisterTargetMachine<X86TargetMachineTPDE>(getTheX86_64Target());
+    RegisterTargetMachine<X86TargetMachineTPDE> X(getTheX86_32Target());
+    RegisterTargetMachine<X86TargetMachineTPDE> Y(getTheX86_64Target());
   }
 };
 
